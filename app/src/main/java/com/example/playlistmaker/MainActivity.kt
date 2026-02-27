85da5da22f8e2a9ecc64e1c3d1de3336eacaf771
Sok-ai <Sokol152433@gmail.com>
2026-02-27 12:48:02 +0300

    Add findViewById for buttons and event handling

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.btn_search)
+        val library = findViewById<Button>(R.id.btn_library)
+        val settings = findViewById<Button>(R.id.btn_settings)
+
+        val clickOnSearch = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Клик по поиску", Toast.LENGTH_SHORT).show()
+            }
+        }
+        search.setOnClickListener(clickOnSearch)
+
+        library.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Клик по медиатеке", Toast.LENGTH_SHORT).show()
+        }
+
+        settings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Клик по настройкам", Toast.LENGTH_SHORT).show()
+        }
     }
 }
