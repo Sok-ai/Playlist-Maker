f2a81903fff683461227350a3d3116e45683e97e
Sok-ai <Sokol152433@gmail.com>
2026-02-27 16:29:27 +0300

    Add back button functionality for settings

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,18 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val btnBack = findViewById<ImageButton>(R.id.btn_settings_to_main)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
