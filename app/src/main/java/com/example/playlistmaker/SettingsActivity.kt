db939d8677f34a775f2ed50e13ae3fe89a847f09
Sok-ai <Sokol152433@gmail.com>
2026-03-07 22:13:28 +0300

    Implement share intent in settings

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,6 +12,28 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val btnBack = findViewById<ImageButton>(R.id.btn_settings_to_main)
+        val share = findViewById<LinearLayout>(R.id.setting_share)
+        val support = findViewById<LinearLayout>(R.id.setting_support)
+        val userAgree = findViewById<LinearLayout>(R.id.setting_user_agreement)
+
+        share.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "https://practicum.yandex.ru/profile/android-developer-plus/"
+                )
+            }
+            startActivity(intent)
+        }
+
+        support.setOnClickListener {
+
+        }
+
+        userAgree.setOnClickListener {
+
+        }
 
         btnBack.setOnClickListener {
             finish()
