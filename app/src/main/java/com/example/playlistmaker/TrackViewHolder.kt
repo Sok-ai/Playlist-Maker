9f1ec2b9116718e63d11cb6ef247968ae83068cf
Sok-ai <Sokol152433@gmail.com>
2026-03-28 19:25:31 +0300

    Add TrackViewHolder with Glide image loading and placeholder

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,31 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+) {
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.trackNameArtist)
+    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+
+    private val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
+    fun bind(model: Track) {
+        Glide
+            .with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.ic_placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(2))
+            .into(trackImage)
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+    }
+}
