d6822aaacf3bbe95d77364a13347baf9fe6b7dd9
Sok-ai <Sokol152433@gmail.com>
2026-04-15 01:52:47 +0300

    Integrate iTunes API search with error handling and empty states

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,44 +2,45 @@ package com.example.playlistmaker
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+    private val retrofit = Retrofit.Builder().baseUrl("https://itunes.apple.com/")
+        .addConverterFactory(GsonConverterFactory.create()).build()
+
+    private val trackService = retrofit.create(SongApi::class.java)
+
+    private val adapter = SongAdapter()
+
+    private lateinit var searchInput: EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var btnBack: ImageButton
+
+    private lateinit var errorLayout: LinearLayout
+    private lateinit var errorImage: ImageView
+    private lateinit var errorMessage: TextView
+    private lateinit var errorButton: Button
+
+    private lateinit var recyclerTask: RecyclerView
+
+    private val songsList = arrayListOf<Song>()
+    private var saveInputText = ""
     private var inputText = INPUT_TEXT_DEF
-    private val trackList: ArrayList<Track> = arrayListOf(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ), Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ), Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ), Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ), Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
 
     override fun onSaveInstanceState(
         outState: Bundle
@@ -48,22 +49,25 @@ class SearchActivity : AppCompatActivity() {
         outState.putString(INPUT_TEXT_KEY, inputText)
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        inputText = savedInstanceState.getString(INPUT_TEXT_KEY, INPUT_TEXT_DEF)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val btnBack = findViewById<ImageButton>(R.id.btn_settings_to_main)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        val searchInput = findViewById<EditText>(R.id.inputEditText)
-        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
-        val recyclerTask = findViewById<RecyclerView>(R.id.recyclerViewTrack)
+        btnBack = findViewById(R.id.btn_settings_to_main)
+        clearButton = findViewById(R.id.clearIcon)
+        searchInput = findViewById(R.id.inputEditText)
+        recyclerTask = findViewById(R.id.recyclerViewTrack)
 
-        recyclerTask.adapter = TrackAdapter(trackList)
+        errorImage = findViewById(R.id.placeholderError)
+        errorMessage = findViewById(R.id.errorMessage)
+        errorButton = findViewById(R.id.btnRefreshNetwork)
+        errorLayout = findViewById(R.id.errorLayout)
+
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        adapter.songs = songsList
+
+        recyclerTask.adapter = adapter
 
         btnBack.setOnClickListener {
             finish()
@@ -75,12 +79,34 @@ class SearchActivity : AppCompatActivity() {
             searchInput.setText("")
             inputMethodManager?.hideSoftInputFromWindow(searchInput.windowToken, 0)
             searchInput.clearFocus()
+            adapter.songs.clear()
+            adapter.notifyDataSetChanged()
         }
 
         searchInput.doOnTextChanged { text, _, _, _ ->
             inputText = text.toString()
             clearButton.visibility = clearButtonVisibility(text)
         }
+
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            searchInput.clearFocus()
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (searchInput.text.isNotEmpty()) {
+                    search(searchInput.text.toString())
+                    true
+                }
+            }
+            false
+        }
+
+        errorButton.setOnClickListener {
+            search(saveInputText)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputText = savedInstanceState.getString(INPUT_TEXT_KEY, INPUT_TEXT_DEF)
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -89,6 +115,58 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun search(input: String) {
+        trackService.getSongs(input).enqueue(object : Callback<SongsResponse> {
+            override fun onResponse(
+                call: Call<SongsResponse?>, response: Response<SongsResponse?>
+            ) {
+                recyclerTask.visibility = View.VISIBLE
+                errorLayout.visibility = View.GONE
+                when (response.code()) {
+                    200 -> {
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            songsList.clear()
+                            songsList.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        } else {
+                            showEmptyError()
+                        }
+                    }
+
+                    else -> {
+                        saveInputText = searchInput.text.toString()
+                        showNetworkError()
+                    }
+                }
+            }
+
+            override fun onFailure(
+                call: Call<SongsResponse?>, t: Throwable
+            ) {
+                saveInputText = searchInput.text.toString()
+                showNetworkError()
+                t.stackTrace
+            }
+
+        })
+    }
+
+    private fun showEmptyError() {
+        recyclerTask.visibility = View.GONE
+        errorButton.visibility = View.GONE
+        errorMessage.setText(R.string.search_empty)
+        errorImage.setImageDrawable(getDrawable(R.drawable.ic_empty_song))
+        errorLayout.visibility = View.VISIBLE
+    }
+
+    private fun showNetworkError() {
+        recyclerTask.visibility = View.GONE
+        errorButton.visibility = View.VISIBLE
+        errorMessage.setText(R.string.network_error)
+        errorImage.setImageDrawable(getDrawable(R.drawable.ic_network_error))
+        errorLayout.visibility = View.VISIBLE
     }
 
     companion object {
