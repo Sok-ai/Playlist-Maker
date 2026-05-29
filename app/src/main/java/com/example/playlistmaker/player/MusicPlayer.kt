e94c5422b999d8535d088734722dcd2fe7a2a262
Sok-ai <Sokol152433@gmail.com>
2026-05-29 14:46:25 +0300

    Add MusicPlayer interface for audio playback control

---
+++ app/src/main/java/com/example/playlistmaker/player/MusicPlayer.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmaker.player
+
+interface MusicPlayer {
+    fun preparePlayer(previewUrl: String, onPrepare: () -> Unit)
+    fun startPlayer()
+    fun pausePlayer()
+    fun isPreparedOrPause(): Boolean
+    fun isPlayer(): Boolean
+    fun currentPosition(): Int
+    fun release()
+    fun setOnCompletionListener(listener: () -> Unit)
+}
