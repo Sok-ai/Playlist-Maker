a9e46e0418ec57737fcc8ba31c92b1e42be4e22b
Sok-ai <Sokol152433@gmail.com>
2026-06-16 12:55:07 +0300

    Inject SongApi dependency into RetrofitClient

--- app/src/main/java/com/example/playlistmaker/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/playlistmaker/data/network/RetrofitClient.kt
@@ -4,19 +4,13 @@ import com.example.playlistmaker.data.NetworkClient
 import com.example.playlistmaker.data.dto.Response
 import com.example.playlistmaker.data.dto.SongSearchRequest
 import com.example.playlistmaker.data.network.api.SongApi
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitClient : NetworkClient {
-    private val retrofit = Retrofit.Builder().baseUrl("https://itunes.apple.com/")
-        .addConverterFactory(GsonConverterFactory.create()).build()
-
-    private val songService: SongApi = retrofit.create(SongApi::class.java)
+class RetrofitClient(private val networkService: SongApi) : NetworkClient {
 
     override fun doRequest(dto: Any): Response {
         when (dto) {
             is SongSearchRequest -> {
-                val resp = songService.searchSongs(dto.expression).execute()
+                val resp = networkService.searchSongs(dto.expression).execute()
                 val body = resp.body() ?: Response()
 
                 return body.apply { resultCode = resp.code() }
