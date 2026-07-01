bb5690e2dbef0e63a347f146e6ebdb88d854b244
Sok-ai <Sokol152433@gmail.com>
2026-07-01 16:59:50 +0300

    refactor(settings): relocate ThemeRepository to settings domain package

--- app/src/main/java/com/example/playlistmaker/settings/data/ThemeRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/settings/data/ThemeRepositoryImpl.kt
@@ -1,7 +1,7 @@
-package com.example.playlistmaker.data.repository
+package com.example.playlistmaker.settings.data
 
 import android.content.SharedPreferences
-import com.example.playlistmaker.domain.api.ThemeRepository
+import com.example.playlistmaker.settings.domain.api.ThemeRepository
 
 const val THEME_KEY = "key_for_switcher_theme"
 class ThemeRepositoryImpl(private val sharedPref: SharedPreferences) : ThemeRepository {
