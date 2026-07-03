bb8dbf58ae8c2bfbb6940f4640d1c780a2a22788
Sok-ai <Sokol152433@gmail.com>
2026-07-03 18:23:15 +0300

    refactor(library): move MediaPlayerImpl to library.data.impl with updated imports

--- app/src/main/java/com/example/playlistmaker/library/data/impl/MediaPlayerImpl.kt
+++ app/src/main/java/com/example/playlistmaker/library/data/impl/MediaPlayerImpl.kt
@@ -1,14 +1,11 @@
-package com.example.playlistmaker.data.player
+package com.example.playlistmaker.library.data.impl
 
 import android.media.MediaPlayer
-import com.example.playlistmaker.domain.api.MusicPlayer
-import com.example.playlistmaker.domain.state.PlayerState.STATE_DEFAULT
-import com.example.playlistmaker.domain.state.PlayerState.STATE_PAUSED
-import com.example.playlistmaker.domain.state.PlayerState.STATE_PLAYING
-import com.example.playlistmaker.domain.state.PlayerState.STATE_PREPARED
+import com.example.playlistmaker.library.domain.api.MusicPlayer
+import com.example.playlistmaker.library.domain.model.PlayerState
 
 class MediaPlayerImpl(private val mediaPlayer: MediaPlayer) : MusicPlayer {
-    private var playerState = STATE_DEFAULT
+    private var playerState = PlayerState.STATE_DEFAULT
     private var onCompletionCallback: (() -> Unit)? = null
 
     override fun preparePlayer(previewUrl: String, onPrepare: () -> Unit) {
@@ -17,27 +14,27 @@ class MediaPlayerImpl(private val mediaPlayer: MediaPlayer) : MusicPlayer {
         mediaPlayer.setVolume(0.6f, 0.6f)
         mediaPlayer.setOnPreparedListener {
             onPrepare()
-            playerState = STATE_PREPARED
+            playerState = PlayerState.STATE_PREPARED
         }
         mediaPlayer.setOnCompletionListener {
-            playerState = STATE_PREPARED
+            playerState = PlayerState.STATE_PREPARED
             onCompletionCallback?.invoke()
         }
     }
 
     override fun startPlayer() {
         mediaPlayer.start()
-        playerState = STATE_PLAYING
+        playerState = PlayerState.STATE_PLAYING
     }
 
     override fun pausePlayer() {
         mediaPlayer.pause()
-        playerState = STATE_PAUSED
+        playerState = PlayerState.STATE_PAUSED
     }
 
-    override fun isPlayer(): Boolean = playerState == STATE_PLAYING
+    override fun isPlayer(): Boolean = playerState == PlayerState.STATE_PLAYING
     override fun isPreparedOrPause(): Boolean =
-        playerState == STATE_PREPARED || playerState == STATE_PAUSED
+        playerState == PlayerState.STATE_PREPARED || playerState == PlayerState.STATE_PAUSED
 
     override fun currentPosition(): Int = mediaPlayer.currentPosition
 
