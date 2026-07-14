f04e8998499b33c4e95bfc5c1ce57c42da5ffe98
Sok-ai <Sokol152433@gmail.com>
2026-07-14 21:38:36 +0300

    feat(search): add Koin DI modules for search feature

---
+++ app/src/main/java/com/example/playlistmaker/search/di/SearchViewModelModule.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker.search.di
+
+import com.example.playlistmaker.search.ui.view_model.SearchViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val searchViewModelModule = module {
+    viewModel {
+        SearchViewModel(interactor = get())
+    }
+}
