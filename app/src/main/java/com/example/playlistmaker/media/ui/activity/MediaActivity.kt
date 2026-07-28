5d9d5b0ac0709e812b0662599425fa8f607e65bf
Sok-ai <Sokol152433@gmail.com>
2026-07-28 19:33:45 +0300

    feat(media): implement tab navigation with ViewPager2 and TabLayoutMediator

--- app/src/main/java/com/example/playlistmaker/media/ui/activity/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/activity/MediaActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaBinding
+import com.example.playlistmaker.media.ui.MediaPageAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMediaBinding
     private lateinit var tabLayoutMediator: TabLayoutMediator
+    private lateinit var pageAdapter: MediaPageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +37,27 @@ class MediaActivity : AppCompatActivity() {
             )
             insets
         }
+        pageAdapter = MediaPageAdapter(supportFragmentManager, lifecycle)
+        binding.pages.adapter = pageAdapter
+
+        tabLayoutMediator = TabLayoutMediator(binding.tabLayout, binding.pages) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.media_name_tag_favorites)
+                else -> tab.text = getString(R.string.media_name_tag_playlists)
+            }
+        }
+
+        tabLayoutMediator.attach()
 
         binding.btnMediaToMain.setOnClickListener {
             finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::tabLayoutMediator.isInitialized) {
+            tabLayoutMediator.detach()
         }
     }
 }
