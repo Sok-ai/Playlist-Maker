ef0df6d288fa0566f438812f54a4e782973a0c5f
Sok-ai <Sokol152433@gmail.com>
2026-08-11 13:27:18 +0300

    feat(deps): add Jetpack Navigation Component dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.viewpager2)
     implementation(libs.material)
     implementation(libs.androidx.activity)
