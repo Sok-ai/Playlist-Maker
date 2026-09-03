3cd8ab3867532650a69075d90bafb2304c5cb046
Sok-ai <Sokol152433@gmail.com>
2026-09-03 19:48:33 +0300

    refactor(network): SongApi already uses suspend function for coroutines

--- app/src/main/java/com/example/playlistmaker/search/data/api/SongApi.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/api/SongApi.kt
@@ -1,7 +1,6 @@
 package com.example.playlistmaker.search.data.api
 
 import com.example.playlistmaker.search.data.dto.SongsSearchResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
