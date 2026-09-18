72e566b80b32d35aac184603ed8c2dd51a8ca12a
Sok-ai <Sokol152433@gmail.com>
2026-09-18 20:32:26 +0300

    refactor(core): remove unused SongEntity import from FavoriteRepository

--- app/src/main/java/com/example/playlistmaker/core/domain/repository/FavoriteRepository.kt
+++ app/src/main/java/com/example/playlistmaker/core/domain/repository/FavoriteRepository.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker.core.domain.repository
 
-import com.example.playlistmaker.core.data.db.entity.SongEntity
 import com.example.playlistmaker.core.domain.model.Song
 import kotlinx.coroutines.flow.Flow
 
