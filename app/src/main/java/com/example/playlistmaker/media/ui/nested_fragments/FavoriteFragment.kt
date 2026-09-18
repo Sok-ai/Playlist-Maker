09c70a20cecf358f0b5699f5e4d447e44deb937e
Sok-ai <Sokol152433@gmail.com>
2026-09-18 19:05:46 +0300

    feat(favorites): implement FavoriteFragment UI with adapter and navigation

--- app/src/main/java/com/example/playlistmaker/media/ui/nested_fragments/FavoriteFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/nested_fragments/FavoriteFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.example.playlistmaker.R
 import com.example.playlistmaker.core.BindingFragment
 import com.example.playlistmaker.databinding.FragmentFavoriteBinding
+import com.example.playlistmaker.library.ui.LibraryFragment
 import com.example.playlistmaker.media.ui.view_model.FavoriteViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteFragment : BindingFragment<FragmentFavoriteBinding>() {
     private val vm: FavoriteViewModel by viewModel<FavoriteViewModel>()
+    private var favoriteAdapter: FavoriteAdapter? = null
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -19,7 +24,58 @@ class FavoriteFragment : BindingFragment<FragmentFavoriteBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        vm.observeNavigateToLibrary().observe(viewLifecycleOwner) {
+            openMusic(it)
+        }
 
+        favoriteAdapter = FavoriteAdapter { song ->
+            vm.onSongClickListener(song)
+        }.apply {
+            vm.observeFavoriteList().observe(viewLifecycleOwner) { listSong ->
+                if (listSong.isNotEmpty()) {
+                    favoriteList = listSong
+                    showContent()
+                } else {
+                    showEmptyScreen()
+                }
+            }
+        }
+
+        binding.recyclerFavorites.adapter = favoriteAdapter
+    }
+
+    private fun showEmptyScreen() {
+        with(binding) {
+            recyclerFavorites.visibility = View.GONE
+            emptyLayout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showContent() {
+        with(binding) {
+            recyclerFavorites.visibility = View.VISIBLE
+            emptyLayout.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        binding.recyclerFavorites.adapter = null
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        favoriteAdapter = null
+    }
+
+    private fun openMusic(songId: Long) {
+        findNavController().navigate(
+            R.id.action_mediaFragment_to_libraryFragment,
+            LibraryFragment.createArgs(songId),
+            navOptions {
+                launchSingleTop = true
+            }
+        )
     }
 
     companion object {
